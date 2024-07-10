de584656543bd8e29ddf14232cf16b1b5967ece1
Israel Oliveira <israeloliveiracontact@gmail.com>
2024-07-10 11:39:13 -0300

    :tada: first commit

---
+++ src/main/java/com/generation/LojaGames2000/LojaGames2000Application.java
@@ -0,0 +1,13 @@
+package com.generation.LojaGames2000;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LojaGames2000Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LojaGames2000Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/generation/LojaGames2000/LojaGames2000ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.generation.LojaGames2000;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LojaGames2000ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
